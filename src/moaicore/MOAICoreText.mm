526f5742490f961d97ade4333f3cb41b1cb80495
Denis Kovacs <den.kovacs@gmail.com>
2013-03-01 18:36:02 -0500

    tried simpler coretext label example. still crashes.

--- src/moaicore/MOAICoreText.mm
+++ src/moaicore/MOAICoreText.mm
@@ -4,7 +4,7 @@
 
 #include <moaicore/MOAICoreText.h>
 
-void writeCoreText(MOAIImage &image)
+void writeCoreTextOLD(MOAIImage &image)
 {
 	CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
 	CGContextRef context = CGBitmapContextCreate(image.GetBitmap(),
@@ -49,4 +49,54 @@ void writeCoreText(MOAIImage &image)
 	CFRelease(string);
 	CFRelease(paragraphStyle);
 	CFRelease(font);
+}
+
+
+void writeCoreText(MOAIImage &image)
+
+{
+	CFStringRef string = (CFStringRef) @"Long";
+	CTFontRef font = CTFontCreateWithName(CFSTR("Helvetica"), 16, NULL);
+
+	CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+//	CGContextRef context = CGBitmapContextCreate(image.GetBitmap(),
+//												 image.GetWidth(),
+//												 image.GetHeight(),
+//												 8,
+//												 image.GetWidth() * 4,
+//												 colorSpace,
+//												 kCGImageAlphaNoneSkipLast );
+
+	CGContextRef context = CGBitmapContextCreate(image.GetBitmap(),
+												 image.GetWidth(),
+												 image.GetHeight(),
+												 8,
+												 image.GetWidth() * 4,
+												 colorSpace,
+												 kCGImageAlphaNoneSkipLast );
+	
+	
+	
+	// Initialize string, font, and context
+	CFStringRef keys[] = { kCTFontAttributeName };
+	CFTypeRef values[] = { font };
+	
+	CFDictionaryRef attributes =
+    CFDictionaryCreate(kCFAllocatorDefault, (const void**)&keys,
+					   (const void**)&values, sizeof(keys) / sizeof(keys[0]),
+					   &kCFTypeDictionaryKeyCallBacks,
+					   &kCFTypeDictionaryValueCallBacks);
+	
+	CFAttributedStringRef attrString =
+    CFAttributedStringCreate(kCFAllocatorDefault, string, attributes);
+	CFRelease(string);
+	CFRelease(attributes);
+	
+	CTLineRef line = CTLineCreateWithAttributedString(attrString);
+	
+	// Set text position and draw the line into the graphics context
+	CGContextSetTextPosition(context, 10.0, 10.0);
+	CTLineDraw(line, context);
+	CFRelease(line);
+	CFRelease(context);
 }
