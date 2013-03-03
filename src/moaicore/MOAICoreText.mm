237e6266a4069c7ddcae8a9b554a55f1b6419205
Denis Kovacs <den.kovacs@gmail.com>
2013-03-03 00:17:05 -0500

    grayscale alpha coretext mask render

--- src/moaicore/MOAICoreText.mm
+++ src/moaicore/MOAICoreText.mm
@@ -8,7 +8,7 @@
 #include <moaicore/MOAICoreText.h>
 
 
-void writeCoreText(MOAIImage &image)
+void writeCoreTextRGBA(MOAIImage &image)
 {
 	int w = image.GetWidth(), h = image.GetHeight();
 	
@@ -16,7 +16,7 @@ void writeCoreText(MOAIImage &image)
 	
 	CGColorSpaceRef colorSpace = CGColorSpaceCreateWithName(kCGColorSpaceGenericRGB);
 
-	CGFloat components[] = { 0.0, 0.0, 0.0, 1.0 };
+	CGFloat components[] = { 1.0, 1.0, 1.0, 1.0 };
 	CGColorRef red = CGColorCreate(colorSpace, components);
 
 	// Create an attributed string
@@ -39,6 +39,7 @@ void writeCoreText(MOAIImage &image)
 												  colorSpace,
 												  kCGImageAlphaPremultipliedFirst | kCGBitmapByteOrder32Host);
 
+	
 //	CGContextSetFillColorWithColor(context, CGColorGetConstantColor(kCGColorBlack));
 //	CGRect rectangle = CGRectMake(0, 0, w,h);
 //	CGContextAddRect(context, rectangle);
@@ -48,14 +49,14 @@ void writeCoreText(MOAIImage &image)
 	CTLineRef line = CTLineCreateWithAttributedString(attrString);
 	CGContextSetTextMatrix(context, CGAffineTransformIdentity);  //Use this one when using standard view coordinates
 	
-	CGContextSetAllowsAntialiasing(context, YES);
-	CGContextSetAllowsFontSmoothing(context, YES);
-	CGContextSetShouldSmoothFonts(context, YES);
-	CGContextSetAllowsFontSubpixelPositioning(context, YES);
-	CGContextSetShouldSubpixelPositionFonts(context, YES);
-	CGContextSetAllowsFontSubpixelQuantization(context, YES);
-	CGContextSetShouldSubpixelQuantizeFonts(context, YES);
-	CGContextSetTextPosition(context, 10, 50);
+	CGContextSetShouldAntialias(context, YES);
+	CGContextSetAllowsFontSmoothing(context, NO);
+	CGContextSetShouldSmoothFonts(context, NO);
+	CGContextSetAllowsFontSubpixelPositioning(context, NO);
+	CGContextSetShouldSubpixelPositionFonts(context, NO);
+	CGContextSetAllowsFontSubpixelQuantization(context, NO);
+	CGContextSetShouldSubpixelQuantizeFonts(context, NO);
+	CGContextSetTextPosition(context, 10, 5);
 	CTLineDraw(line, context);
 	
 	// Clean up
@@ -64,4 +65,60 @@ void writeCoreText(MOAIImage &image)
 	CFRelease(font);
 	CFRelease(context);
 
+}
+
+
+
+void writeCoreText(MOAIImage &image)
+{
+	int w = image.GetWidth(), h = image.GetHeight();
+	
+	CTFontRef font = CTFontCreateWithName(CFSTR("Helvetica"), 10.0, NULL);
+	
+	CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceGray();
+	
+	CGFloat components[] = { 1.0, 1.0, 1.0, 1.0 };
+	CGColorRef red = CGColorCreate(colorSpace, components);
+	
+	// Create an attributed string
+	CFStringRef keys[] = { kCTFontAttributeName, kCTForegroundColorAttributeName };
+	CFTypeRef values[] = { font, red };
+	CFDictionaryRef attr = CFDictionaryCreate(NULL, (const void **)&keys, (const void **)&values,
+											  sizeof(keys) / sizeof(keys[0]), &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+	CFAttributedStringRef attrString = CFAttributedStringCreate(NULL, CFSTR("Hello World"), attr);
+	CFRelease(attr);
+	
+	
+    int bitmapBytesPerRow   = (w * 1);
+    int bitmapByteCount     = (bitmapBytesPerRow * h);
+	
+    CGContextRef context = CGBitmapContextCreate (image.GetBitmap(),
+												  w,
+												  h,
+												  8,
+												  bitmapBytesPerRow,
+												  colorSpace,
+												  kCGImageAlphaOnly);
+	
+
+	// Draw the string
+	CTLineRef line = CTLineCreateWithAttributedString(attrString);
+	CGContextSetTextMatrix(context, CGAffineTransformIdentity);  //Use this one when using standard view coordinates
+	
+	CGContextSetShouldAntialias(context, YES);
+	CGContextSetAllowsFontSmoothing(context, YES);
+	CGContextSetShouldSmoothFonts(context, YES);
+	CGContextSetAllowsFontSubpixelPositioning(context, YES);
+	CGContextSetShouldSubpixelPositionFonts(context, YES);
+	CGContextSetAllowsFontSubpixelQuantization(context, NO);
+	CGContextSetShouldSubpixelQuantizeFonts(context, NO);
+	CGContextSetTextPosition(context, 10, 5);
+	CTLineDraw(line, context);
+	
+	// Clean up
+	CFRelease(line);
+	CFRelease(attrString);
+	CFRelease(font);
+	CFRelease(context);
+	
 }
